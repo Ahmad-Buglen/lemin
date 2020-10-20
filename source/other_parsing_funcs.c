1730a7ac8212a0520efe3f1ebc3b1448c88af651
Serge Opanasenko <sergey.opanasenko@gmail.com>
2020-10-20 13:01:08 +0300

    map can start with comments FIXED

--- source/other_parsing_funcs.c
+++ source/other_parsing_funcs.c
@@ -6,7 +6,7 @@
 /*   By: Alkor <Alkor@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/19 11:51:50 by bsausage          #+#    #+#             */
-/*   Updated: 2020/10/16 19:08:52 by Alkor            ###   ########.fr       */
+/*   Updated: 2020/10/20 12:58:31 by Alkor            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int			check_num_of_ants_line(char *line)
 	return (1);
 }
 
-static void	check_first_line(t_lem_in *lemin)
+static int	check_first_line(t_lem_in *lemin)
 {
 	if (lemin->line[0] == '\0')
 		close_program(lemin, "empty line");
@@ -40,23 +40,29 @@ static void	check_first_line(t_lem_in *lemin)
 	if (lemin->line[0] == '#')
 	{
 		ft_memdel((void**)&lemin->line);
-		close_program(lemin, "map must starts with num of ants");
+		return (1);
 	}
 	if (!check_num_of_ants_line(lemin->line))
 		close_program(lemin, "wrong num of ants line");
+	return (0);
 }
 
 void		get_num_of_ants(t_lem_in *lemin)
 {
 	long	num;
 	char	*end;
+	int		gnl;
 
-	if (get_next_line(0, &lemin->line) <= 0)
-		close_program(lemin,\
-		"empty file or map doesn't contain rooms and link part");
-	lemin->line_num++;
-	add_line_to_farm_list(lemin);
-	check_first_line(lemin);
+	while ((gnl = get_next_line(0, &lemin->line)))
+	{
+		if (gnl <= 0)
+			close_program(lemin,\
+			"empty file or map doesn't contain rooms and link part");
+		lemin->line_num++;
+		add_line_to_farm_list(lemin);
+		if (!(check_first_line(lemin)))
+			break ;
+	}
 	end = lemin->line;
 	num = ft_strtol(end, &end);
 	if (num <= 0 || num > INT_MAX)
